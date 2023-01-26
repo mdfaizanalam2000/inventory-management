3ba0d2a1c69fa968a1bbdfda2baf5c1544b592d9
MD Faizan Alam <mac020@nist.edu>
2023-01-26 13:19:00 +0530

    updated table.js

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -5,23 +5,26 @@ export default function Heading(props) {
         <div className='container'>
             <h2 className='text-center'>Items Detail</h2>
             {props.todos.length === 0 ? "Nothing to display! Add items to display here" :
-                <table className="table">
-                    <thead className='bg-primary text-light'>
-                        <tr>
-                            <th scope="col">SNo.</th>
-                            <th scope="col">Item Name</th>
-                            <th scope="col">Date of Service</th>
-                            <th scope="col">Owner Name</th>
-                            <th scope="col">Vendor Name</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.todos.map((todo) => {
-                            return <Todo todo={todo} key={todo.sno} onDelete={props.onDelete} />
-                        })}
-                    </tbody>
-                </table>}
+                <div className='table-responsive'>
+                    <table className="table">
+                        <thead className='bg-primary text-light'>
+                            <tr>
+                                <th scope="col">SNo.</th>
+                                <th scope="col">Item Name</th>
+                                <th scope="col">Date of Service</th>
+                                <th scope="col">Owner Name</th>
+                                <th scope="col">Vendor Name</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.todos.map((todo) => {
+                                return <Todo todo={todo} key={todo.sno} onDelete={props.onDelete} />
+                            })}
+                        </tbody>
+                    </table>
+                </div>}
+
         </div>
     )
 }
